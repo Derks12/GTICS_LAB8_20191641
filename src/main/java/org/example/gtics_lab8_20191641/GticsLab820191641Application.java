77c24e6359b66bf53727c5043c1f1cc13972ff44
Derks12 <148168249+Derks12@users.noreply.github.com>
2024-06-12 19:23:45 -0500

    aaaaaa

---
+++ src/main/java/org/example/gtics_lab8_20191641/GticsLab820191641Application.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab8_20191641;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab820191641Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab820191641Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gtics_lab8_20191641/GticsLab820191641ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab8_20191641;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab820191641ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
